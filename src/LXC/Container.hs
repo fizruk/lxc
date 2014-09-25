d713337667c1edfa7c6897d9f40f207bfba35b77
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2014-09-25 14:25:52 +0400

    [container] create without bdev options + flags and comments

--- src/LXC/Container.hs
+++ src/LXC/Container.hs
@@ -1,13 +1,25 @@
 module LXC.Container where
 
 import Bindings.LXC.Container
+import Bindings.LXC.Sys.Types
 
 import Control.Applicative
 import Control.Monad
 
-import Foreign.C.String (withCString)
-import Foreign.Ptr (nullPtr, Ptr)
+import Data.Bits
+import Data.List (foldl')
 
+import Foreign.C.Types
+import Foreign.C.String (withCString, newCString, CString)
+import Foreign.Marshal.Array (withArray0)
+import Foreign.Ptr (nullPtr, Ptr, FunPtr)
+import Foreign.Storable
+
+type ContainerCreateFn = Ptr C'lxc_container -> CString -> CString -> Ptr C'bdev_specs -> CInt -> Ptr CString -> IO CBool
+foreign import ccall "dynamic"
+  mkCreateFn :: FunPtr ContainerCreateFn -> ContainerCreateFn
+
+-- | Options for 'clone' operation.
 data CloneOption
   = CloneKeepName        -- ^ Do not edit the rootfs to change the hostname.
   | CloneKeepMacAddr     -- ^ Do not change the MAC address on network interfaces.
@@ -17,22 +29,56 @@ data CloneOption
   | CloneMaxFlags        -- ^ Number of @LXC_CLONE_*@ flags.
   deriving (Eq, Ord)
 
+-- | Options for 'create' operation.
 data CreateOption
   = CreateQuiet          -- ^ Redirect @stdin@ to @/dev/zero@ and @stdout@ and @stderr@ to @/dev/null@.
   | CreateMaxFlags       -- ^ Number of @LXC_CREATE*@ flags.
   deriving (Eq, Ord)
 
-newtype Container = Container { getContainer :: Ptr C'lxc_container }
+-- | Turn 'CreateOption' into a bit flag.
+createFlag :: CreateOption -> CInt
+createFlag CreateQuiet    = c'LXC_CREATE_QUIET
+createFlag CreateMaxFlags = c'LXC_CREATE_MAXFLAGS
 
--- | Create a new container.
---
-new :: String           -- ^ Name to use for the container.
-    -> Maybe FilePath   -- ^ Full path to configuration file to use.
-    -> IO Container     -- ^ Newly allocated container.
-new name configPath = do
+-- | Collect flags in a single integer value.
+mkFlags :: (a -> CInt) -> [a] -> CInt
+mkFlags f = foldl' (.|.) 0 . map f
+
+-- | Container object.
+newtype Container = Container {
+  getContainer :: Ptr C'lxc_container   -- ^ A pointer to @lxc_container@ structure.
+}
+
+-- | Allocate a new container.
+mkContainer :: String           -- ^ Name to use for the container.
+            -> Maybe FilePath   -- ^ Full path to configuration file to use.
+            -> IO Container     -- ^ Newly allocated container.
+mkContainer name configPath = do
   c <- withCString name $ \cname ->
           case configPath of
             Nothing -> c'lxc_container_new cname nullPtr
             Just s  -> withCString s $ c'lxc_container_new cname
   when (c == nullPtr) $ error "failed to allocate new container"
   return $ Container c
+
+-- | Create a container.
+create :: Container         -- ^ Container (with lxcpath, name and a starting configuration set).
+       -> String            -- ^ Template to execute to instantiate the root filesystem and adjust the configuration.
+       -> bDevType          -- ^ Backing store type to use (if @NULL@, @dir@ will be used).
+       -> bDevSpecs         -- ^ Additional parameters for the backing store (for example LVM volume group to use).
+       -> [CreateOption]    -- ^ 'CreateOption' options. /Note: LXC 1.0 supports only @CreateQuiet@ option./
+       -> [String]          -- ^ Arguments to pass to the template.
+       -> IO Bool           -- ^ @True@ on success. @False@ otherwise.
+create c t bdevtype bdevspecs flags argv = do
+  cargv <- mapM newCString argv
+  r     <- withArray0 nullPtr cargv $ \cargv' ->
+              withCString t $ \ct -> do
+                fn <- peek $ p'lxc_container'create $ getContainer c
+                mkCreateFn fn
+                  (getContainer c)
+                  ct
+                  nullPtr
+                  nullPtr
+                  (mkFlags createFlag flags)
+                  cargv'
+  return (r == 1)
